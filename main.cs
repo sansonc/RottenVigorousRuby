a366645456110b872b689fa59719340de6d8b39d
sansonc <sanson.cruz@gmail.com>
2024-05-25 19:56:04 +0000

    test

--- main.cs
+++ main.cs
@@ -5,10 +5,10 @@ class Program
    static void Main () 
   {
     const double cmperinch = 2.54;
-    double inches = 5;
+    double inches = 15994;
 
     double centimeters = inches * cmperinch;
     
-    Console.WriteLine($"{centimeters} centimeters.");
+    Console.WriteLine($"{inches} inches is {centimeters} centimeters.");
   }
 }
